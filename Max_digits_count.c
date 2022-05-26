87ff8d208857f8259403a48b20878cb01a23abee
21A91A05D8 <97424615+21A91A05D8@users.noreply.github.com>
2022-05-26 11:23:33 +0530

    #include<stdio.h> int main() {     int n,arr[100],c=0,i,r,j,max=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         c=0;         while(arr[i])         {             r=arr[i]%10;             c++;             arr[i]/=10;         }         if(c>max)         {             max=c;         }         arr[i]=c;     }     c=0;     for(i=0;i<n;i++)     {         if(max==arr[i])         {             c++;         }     }     printf("%d",c); }

---
+++ Max_digits_count.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[100],c=0,i,r,j,max=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        c=0;
+        while(arr[i])
+        {
+            r=arr[i]%10;
+            c++;
+            arr[i]/=10;
+        }
+        if(c>max)
+        {
+            max=c;
+        }
+        arr[i]=c;
+    }
+    c=0;
+    for(i=0;i<n;i++)
+    {
+        if(max==arr[i])
+        {
+            c++;
+        }
+    }
+    printf("%d",c);
+}
