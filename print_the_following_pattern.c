96f5ccdeff933c62651e7ebdb9a7dbd25d07c1ac
21A91A05D8 <97424615+21A91A05D8@users.noreply.github.com>
2022-11-29 14:09:29 +0530

    #include<stdio.h> int main() {  int n,i,j,k;  scanf("%d" ,&n);  for(i=n;i>=1;i--)  {  for(j=1;j<=i-1;j++)  {  printf(" " );  }  for(k=1;k<=n;k++) {  if(i==1||i==n||k==1||k==n) { printf("*" );  }  else {  printf(" " ); }  }  printf(" " );  } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,21 +1,25 @@
+
 #include<stdio.h>
 int main()
 {
-    int n;
-    scanf("%d",&n);
-    int i,j,k;
-    for(i=1;i<=n;i++)
-    {
-        for(j=n;j>i;j--)
-        {
-            printf(" ");
-        }
-        for(k=1;k<=(2*i)-1;k++)
-        {
-            printf("%d",i);
-        }
-        printf("
-");
-    }
-    
+ int n,i,j,k;
+ scanf("%d" ,&n);
+ for(i=n;i>=1;i--)
+ {
+ for(j=1;j<=i-1;j++)
+ {
+ printf(" " ); 
+ }
+ for(k=1;k<=n;k++) 
+{
+ if(i==1||i==n||k==1||k==n) 
+{ 
+printf("*" );
+ }
+ else {
+ printf(" " ); }
+ }
+ printf("
+" );
+ }
 }
